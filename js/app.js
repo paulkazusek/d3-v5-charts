df39040c03f5f6c40eb560d22930a8b7612bba96
Paul Kazusek <paul.kazusek@gmail.com>
2019-06-18 10:45:52 +0200

    add line to linechart and refactor code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-var height = 800,
-    width = 800,
-    margin = 25;
+var height = 800
+var width = 800
+var margin = { top: 30, left: 30, right: 30, bottom: 30 }
+
+var maxY = 100
+const data = [{ x: 0, y: 10 }, { x: 10, y: 50 }, { x: 20, y: 25 }, { x: 25, y: 30 }]
+
+var yScale, xScale
+
+const curves = [
+    'curveLinear',
+    'curveBasis',
+    'curveBundle',
+    'curveCardinal',
+    'curveCatmullRom',
+    'curveMonotoneX',
+    'curveMonotoneY',
+    'curveNatural',
+    'curveStep',
+    'curveStepAfter',
+    'curveStepBefore',
+    'curveBasisClosed'
+]
 
 var svg = d3.select("body").append("svg")
     .attr("class", "axis")
@@ -8,19 +28,17 @@ var svg = d3.select("body").append("svg")
     .attr("height", height);
 
 function renderXAxis() {
-    var axisLength = width - 2 * margin;
-
-    var scale = d3.scaleLinear()
+    xScale = d3.scaleLinear()
         .domain([0, 100])
-        .range([0, axisLength]);
+        .range([0, quadrantWidth()]);
 
     var xAxis = d3.axisBottom()
-        .scale(scale);
+        .scale(xScale);
 
     svg.append("g")
         .attr("class", "x-axis")
         .attr("transform", function () {
-            return "translate(" + margin + "," + (height - margin) + ")";
+            return "translate(" + xStart() + "," + yStart() + ")";
         })
         .call(xAxis);
 
@@ -30,23 +48,21 @@ function renderXAxis() {
         .attr("x1", 0)
         .attr("y1", 0)
         .attr("x2", 0)
-        .attr("y2", - (height - 2 * margin));
+        .attr("y2", - quadrantHeight());
 }
 
 function renderYAxis() {
-    var axisLength = height - 2 * margin;
-
-    var scale = d3.scaleLinear()
+    yScale = d3.scaleLinear()
         .domain([100, 0])
-        .range([0, axisLength]);
+        .range([0, quadrantHeight()]);
 
     var yAxis = d3.axisLeft()
-        .scale(scale);
+        .scale(yScale);
 
     svg.append("g")
         .attr("class", "y-axis")
         .attr("transform", function () {
-            return "translate(" + margin + "," + margin + ")";
+            return "translate(" + xStart() + "," + yEnd() + ")";
         })
         .call(yAxis);
 
@@ -55,9 +71,72 @@ function renderYAxis() {
         .classed("grid-line", true)
         .attr("x1", 0)
         .attr("y1", 0)
-        .attr("x2", axisLength)
+        .attr("x2", quadrantWidth())
         .attr("y2", 0);
+}
+
+function renderLine() {
+    var line = d3.line()
+        .x(function (d) { return xScale(d.x); })
+        .y(function (d) { return yScale(d.y); })
+        .curve(d3[curves[0]]);
+
+    svg.append('path')
+        .datum(data)
+        .attr('class', 'data-line glowed')
+        .style('stroke', '#ff0000')
+        .style('stroke-width', 2)
+        .style('fill', 'none')
+        //.attr('d', line)
+        .attr("d", function (d) { return line(d); })
+        .attr("transform", "translate("
+            + xStart() + ","
+            + yEnd() + ")")
+
+}
+
+function renderDots() {
+    var circles = svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('class', 'circle')
+        .attr('cx', (d) => xScale(d.x))
+        .attr('cy', (d) => yScale(d.y))
+        .attr('r', 4)
+        .style('fill', '#ff0000')
+        .style('stroke', '#000000')
+        .style('stroke-width', 2)
+        .attr("transform", "translate("
+            + xStart() + ","
+            + yEnd() + ")")
+}
+
+function xStart() {
+    return margin.left;
+}
+
+function yStart() {
+    return height - margin.bottom;
+}
+
+function xEnd() {
+    return width - margin.right;
+}
+
+function yEnd() {
+    return margin.top;
+}
+
+function quadrantWidth() {
+    return width - margin.left - margin.right;
+}
+
+function quadrantHeight() {
+    return height - margin.top - margin.bottom;
 }
 
 renderYAxis();
 renderXAxis();
+renderLine();
+renderDots();
